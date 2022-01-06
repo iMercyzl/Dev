ec9d83dd5c8b3a535aa6f9b4e2677d243d72f4dd
iMercyzl <pedroluizct3@gmail.com>
2022-01-06 15:49:20 -0300

    exercicio de pares e palindromo

--- DIO/Projetinhos/palindromo.js
+++ DIO/Projetinhos/palindromo.js
@@ -1,7 +1,21 @@
 function verificaPalindromo(string) {
-    if(!string) return;
+    if(!string) return "string inexistente";
 
     string.split("").reverse().join("") === string;
 }
 
 console.log(verificaPalindromo("ovo"))
+
+function varificaPalindromo2(string) {
+    if (!string) return "string inexistente";
+
+    for(let i = 0; i< string.length / 2; i++) {
+        if(string[i] !== string[string.length -1 - i]){
+            return false;
+        }
+        
+    }
+    return true;
+}
+
+console.log(verificaPalindromo2("abba"));
