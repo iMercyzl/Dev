dee4ad156616b0e7044072024461745499e023eb
iMercyzl <pedroluizct3@gmail.com>
2022-01-23 11:41:46 -0300

    atualização Repositórios

---
+++ curso em video/JS/exercicios/Vespetino/Readme.md
@@ -0,0 +1,13 @@
+# Vespetino
+
+Programa para mostrar a hora do dia e se já é Dia, Tarde e Noite.
+
+
+
+Criador: Curso em Video
+
+
+
+## Exemplo
+
+![Exercício Vespetino](./Vespetino.gif)
--- curso em video/JS/exercicios/Vespetino/assets/script/script.js
+++ curso em video/JS/exercicios/Vespetino/assets/script/script.js
@@ -3,25 +3,25 @@ function carregar () {
     var bdtn = window.document.querySelector('div#bdtn')
     var img = window.document.querySelector('img#imagem')
     var data = new Date()
-    var hora = data.getHours()
+    //var hora = data.getHours()
     var min = data.getMinutes()
 
-    //var hora = 13
+    var hora = 19
     //bdtn.innerHTML = `Agora são ${hora}:${min}.`
     if(hora >=6 && hora <12) {
         msg.innerHTML = 'BOM DIA!'
         bdtn.innerHTML = `Agora são ${hora}:${min}.`
-        img.src = 'dia.png'
+        img.src = 'assets/imagens/dia.png'
         document.body.style.background = '#cfe0e7'
     } else if(hora >=12 && hora <18) {
         msg.innerHTML = 'BOA TARDE!'
         bdtn.innerHTML = `Agora são ${hora}:${min}.`
-        img.src = 'tarde.png'
+        img.src = 'assets/imagens/tarde.png'
         document.body.style.background = '#e2a657'
     } else {
         msg.innerHTML = '<p>BOA NOITE!</p>'
         bdtn.innerHTML = `<p>Agora são ${hora}:${min}.</p>`
-        img.src = 'noite.png'
+        img.src = 'assets/imagens/noite.png'
         document.body.style.background = '#584264'
         //document.
     }
